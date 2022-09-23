c99e2f772cc08d0219bc3135c379577101456fe1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-23 16:53:32 +0900

    sequelize syc, nodemon 설치 완료

--- prepare/backend/app.js
+++ prepare/backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 
-const postRouter = reuqire('./routes/post')
-
+const postRouter = require('./routes/post')
+const db = require('./models');
 const app = express()
 
+db.sequelize.sync()
+  .then(() => {
+    console.log('sequelize 서버 연결 완료')
+  })
+  .catch((e) => {
+    console.error(e)
+  })
 
 app.use('/post', postRouter)
+
 app.listen(3065, () => {
   console.log('서버 실행중')
 })
--- prepare/backend/models/index.js
+++ prepare/backend/models/index.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize');
-
+const comment = require('./comment');
+const hashtag = require('./hashtag');
+const image = require('./image');
+const post = require('./post');
+const user = require('./user');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -7,7 +11,11 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-
+db.Comment = comment;
+db.Hashtag = hashtag;
+db.Image = image;
+db.Post = post;
+db.User = user;
 
 Object.keys(db).forEach(modelName => {
   db[modelName].init(sequelize);
