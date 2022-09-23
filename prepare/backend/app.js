be0e26bf137167ecbae132909983058edddce64f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-23 18:09:13 +0900

    passport 를 이용한 회원가입, 로그인 구현 완료

--- prepare/backend/app.js
+++ prepare/backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const postRouter = require('./routes/post')
 const userRouter = require('./routes/user')
 
 const db = require('./models');
-const app = express()
+const passport = require('passport')
+const passportConfig = require('./passport');
 
+dotenv.config()
+const app = express()
 db.sequelize.sync()
   .then(() => {
     console.log('sequelize 서버 연결 완료')
@@ -13,7 +19,7 @@ db.sequelize.sync()
   .catch((e) => {
     console.error(e)
   })
-
+passportConfig();
 
 app.use(cors({
   origin: true,
@@ -21,6 +27,15 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+}));
+app.use(passport.initialize());
+//passport.initialize에서 req에 isAuthenticated login logout등을 추가
+app.use(passport.session());
 
 app.use('/post', postRouter)
 app.use('/user', userRouter)
--- prepare/backend/routes/user.js
+++ prepare/backend/routes/user.js
@@ -1,16 +1,69 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const { User } = require('./models')
+const passport = require('passport');
+ 
+const { User, Post } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router()
 
-router.post('/', async (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      if (info) {
+        return res.status(401).send(info.reason);
+      }
+      return req.login(user, async (loginErr) => { // 로그인상태 쿠키를 프론트로 보낸다.
+        if (loginErr) {
+            console.error(loginErr);
+            return next(loginErr);
+        }
+        const fullUserWithoutPassword = await User.findOne({
+            where: { id: user.id },
+            attributes: {
+              exclude: ['password']
+            },
+            include: [{
+              model: Post,
+              attributes: ['id'],
+            }, {
+              model: User,
+              as: 'Followings',
+              attributes: ['id'],
+            }, {
+              model: User,
+              as: 'Followers',
+              attributes: ['id'],
+            }]
+        })
+        return res.status(200).json(fullUserWithoutPassword);
+      });
+  })(req, res, next);
+})
+
+router.post('/logout', isLoggedIn, (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.send('ok');
+  });
+  
+router.post('/', isNotLoggedIn, async (req, res, next) => { // POST /user/
   try{
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      }
+    });
+    if (exUser) {
+      return res.status(403).send('이미 사용 중인 아이디입니다.');
+    }
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
     await User.create({
-  
-          email: req.body.email,
-          nickname: req.body.nickname,
-          password: hashedPassword,
+            email: req.body.email,
+            nickname: req.body.nickname,
+            password: hashedPassword,
     });
     res.send('ok')
   } catch(e) {
