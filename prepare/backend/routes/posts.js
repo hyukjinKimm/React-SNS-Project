3f4e8da8a88679db7e98687b4340068481e06835
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-29 23:30:21 +0900

    게시글 리트윗 라우터 추가

--- prepare/backend/routes/posts.js
+++ prepare/backend/routes/posts.js
@@ -4,30 +4,42 @@ const router = express.Router()
 
 router.get('/', async (req, res, next) => {
     try{
-      const posts = await Post.fideAll({
+      const where = {};
+      if (parseInt(req.query.lastId, 10)) { // 초기 로딩이 아닐 때
+        where.id = { [Op.lt]: parseInt(req.query.lastId, 10)}
+      } // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+      const posts = await Post.findAll({
+        where,
         limit: 10,
-        // offset: 0, // 1 ~ 10 게시글 추가 삭제시 문제 발생 
         order: [
-            ['createdAt', 'DESC'],
-            [Comment, 'createdAt', 'DESC']
-        ], // 최신 게시글 부터 가져옴
-        
+          ['createdAt', 'DESC'],
+          [Comment, 'createdAt', 'DESC'],
+        ],
         include: [{
-          model: User, // 게시글 작성자
-          attributes: ['id', 'nickname']
+          model: User,
+          attributes: ['id', 'nickname'],
         }, {
-          model: Image
+          model: Image,
         }, {
           model: Comment,
           include: [{
-            model: User, // 댓글 작성자
-            attributes: ['id', 'nickname']
-          }]
+            model: User,
+            attributes: ['id', 'nickname'],
+          }],
         }, {
           model: User, // 좋아요 누른 사람
           as: 'Likers',
-          attributes: ['id']
-        }]
+          attributes: ['id'],
+        }, {
+          model: Post,
+          as: 'Retweet',
+          include: [{
+            model: User,
+            attributes: ['id', 'nickname'],
+          }, {
+            model: Image,
+          }]
+        }],
       });
       res.status(200).json(posts);
     } catch(error){
