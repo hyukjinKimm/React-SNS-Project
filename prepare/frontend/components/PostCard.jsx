245a715d96b2eab961c75774a64027cc28abae74
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-04 04:26:23 +0900

    프론트엔드/ 게시글에서 해쉬태그 모양 감지해서 링크로 바꾸기

--- prepare/frontend/components/PostCard.jsx
+++ prepare/frontend/components/PostCard.jsx
@@ -5,6 +5,7 @@ import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, Hear
 import { useSelector } from 'react-redux'
 import PostImages from './PostImages'
 import CommentForm from './CommentForm'
+import PostCardContent from './PostCardContent'
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false)
   const [commentFormOpend, setCommentFormOpend] = useState(false)
@@ -49,7 +50,7 @@ const PostCard = ({ post }) => {
         <Card.Meta 
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpend &&
