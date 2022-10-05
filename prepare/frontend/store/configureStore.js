edc3e06c49ddefb5041aa1196b5679edb4dc8f51
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-05 12:53:22 +0900

    redux-saga 설치와 generator 함수 사용법 숙지

--- prepare/frontend/store/configureStore.js
+++ prepare/frontend/store/configureStore.js
@@ -1,16 +1,23 @@
 import { createWrapper }  from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
 import reducer from '../reducers'
-import thunkMiddleware from 'redux-thunk'
+import rootSaga from '../sagas'
+const loggerMiddleware = ({ dispatch, getstate}) => (next) => (action) => {
+  console.log(action)
+  return next(action)
+}
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware]
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [sagaMiddleware,loggerMiddleware]
   const enhancer = process.env.NODE_ENV === 'productino' 
   ? compose(applyMiddleware(...middlewares))
   : composeWithDevTools(applyMiddleware(...middlewares))
 
   const store = createStore(reducer, enhancer)
+  store.sagaTask = sagaMiddleware.run(rootSaga)
   return store
 }
 const wrapper = createWrapper(configureStore, {
